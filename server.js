d5a1b68a18bef95ab17481ab897fd5d66a6296eb
Sugar Shane <scrisostomo17@gmail.com>
2021-08-07 02:45:03 -0400

    heroku push

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+//parse incoming string or array data 
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data 
+app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -41,17 +53,93 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findByID(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    //our function's main code will go here!
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    //return finished code to post route for response 
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animals.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animals.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || typeof animals.personalityTraits !== 'string') {
+        return false;
+    } 
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+      results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findByID(req.params.id, animals);
+    if (results) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }   
 });
+
+app.post('/api/animals', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back 
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get('zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log('API server now on port 3001!');
 });
 
-const { animals } = require('./data/animals.json');
 
+
+
+
+//website-for Heroku:  https://blooming-harbor-99176.herokuapp.com/api/animals
